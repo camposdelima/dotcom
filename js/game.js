b0bc3310cfc7329567dfde863342d4180d721788
Cristiano Campos <cristiano.campos@outlook.com>
2026-06-29 21:09:30 -0300

    fix: restore missing getProgress and undoMove functions in game.js

--- js/game.js
+++ js/game.js
@@ -151,3 +151,72 @@ function checkWin(game, player) {
 
   return null;
 }
+
+function getProgress(game, player) {
+  const { rows, cols } = game;
+  const visited = new Set();
+  const key = (r, c) => r * cols + c;
+  let bestSpan = 0;
+
+  for (let sr = 0; sr < rows; sr++) {
+    for (let sc = 0; sc < cols; sc++) {
+      if (visited.has(key(sr, sc))) continue;
+
+      const hasEdge =
+        (sc < cols - 1 && game.horizontalEdges[sr][sc] === player) ||
+        (sc > 0 && game.horizontalEdges[sr][sc - 1] === player) ||
+        (sr < rows - 1 && game.verticalEdges[sr][sc] === player) ||
+        (sr > 0 && game.verticalEdges[sr - 1][sc] === player);
+
+      if (!hasEdge && sc > 0) continue;
+
+      const queue = [[sr, sc]];
+      visited.add(key(sr, sc));
+      let minC = sc, maxC = sc;
+
+      while (queue.length) {
+        const [r, c] = queue.shift();
+
+        if (c < cols - 1 && game.horizontalEdges[r][c] === player) {
+          const nk = key(r, c + 1);
+          if (!visited.has(nk)) { visited.add(nk); queue.push([r, c + 1]); if (c + 1 > maxC) maxC = c + 1; }
+        }
+        if (c > 0 && game.horizontalEdges[r][c - 1] === player) {
+          const nk = key(r, c - 1);
+          if (!visited.has(nk)) { visited.add(nk); queue.push([r, c - 1]); if (c - 1 < minC) minC = c - 1; }
+        }
+        if (r < rows - 1 && game.verticalEdges[r][c] === player) {
+          const nk = key(r + 1, c);
+          if (!visited.has(nk)) { visited.add(nk); queue.push([r + 1, c]); }
+        }
+        if (r > 0 && game.verticalEdges[r - 1][c] === player) {
+          const nk = key(r - 1, c);
+          if (!visited.has(nk)) { visited.add(nk); queue.push([r - 1, c]); }
+        }
+      }
+
+      const span = maxC - minC;
+      if (span > bestSpan) bestSpan = span;
+    }
+  }
+
+  return bestSpan;
+}
+
+function undoMove(game) {
+  if (game.moves.length === 0) return false;
+
+  const move = game.moves.pop();
+  if (move.orientation === 'h') {
+    game.horizontalEdges[move.row][move.col] = null;
+  } else {
+    game.verticalEdges[move.row][move.col] = null;
+  }
+
+  game.currentPlayer = move.player;
+  game.winner = null;
+  game.winningPath = null;
+  game.winningEdges = null;
+
+  return true;
+}
